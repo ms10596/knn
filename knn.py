32b7caac99c148385db7eb8367a6df944c5ff69a
Mohamed Sayed <ms10596@gmail.com>
2018-11-28 09:35:24 +0200

    feat: plotting accuracy

--- knn.py
+++ knn.py
@@ -2,16 +2,21 @@ import numpy as np
 
 
 class KNN:
-    def __init__(self, x_train, y_train, k):
+    def __init__(self, x_train, y_train):
         self.x_train = x_train
         self.y_train = y_train
-        self.k = k
+        self.k = 1
 
     def predict(self, x_test):
         def euclidean(b):
             return np.sum(np.sqrt(np.power(x_test - b[0], 2)))
 
         sorted_x_train, sorted_y_train = zip(*sorted(zip(self.x_train, self.y_train), key=euclidean))
-        choosen_k = sorted_y_train[0:self.k]
-        return max(choosen_k, key=choosen_k.count)
+        chosen_k = sorted_y_train[0:self.k]
+        return max(chosen_k, key=chosen_k.count)
 
+    def accuracy(self, x_test, y_test, k):
+        self.k = k
+        y_hat = list(map(self.predict, x_test))
+        true_no = np.count_nonzero(y_test == y_hat)
+        return true_no
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import pandas as pd
-import numpy as np
 from read_data import read_data
 from knn import KNN
-
+import matplotlib.pyplot as plt
 if __name__ == '__main__':
     x_train, y_train = read_data("TrainData.txt")
     x_test, y_test = read_data("TestData.txt")
 
-    clf = KNN(x_train, y_train, 3)
-    clf.predict(x_train[1])
+    clf = KNN(x_train, y_train)
+    accuracies = []
+    for i in range(1, 10):
+        print("k value:", i)
+        correctly_classified = clf.accuracy(x_test, y_test, i)
+        accuracy = correctly_classified / len(y_test)
+        accuracies.append(accuracy)
+        print("Number of correctly classified instances:", correctly_classified)
+        print("Number of instances:", len(y_test))
+        print("Accuracy:", accuracy)
+        print("\n\n")
+
+plt.plot(accuracies)
+plt.show()
